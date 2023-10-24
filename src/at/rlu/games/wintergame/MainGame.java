74fd12ddc16a8677a8b4bfc4bd3353001c215597
lukasrauch <lukas.rauch2005@gmail.com>
2023-10-24 10:24:51 +0200

    first rectangle

--- src/at/rlu/games/wintergame/MainGame.java
+++ src/at/rlu/games/wintergame/MainGame.java
@@ -4,7 +4,11 @@ import org.newdawn.slick.*;
 import org.newdawn.slick.tests.AnimationTest;
 
 public class MainGame extends BasicGame {
-    private int x,y;
+    private int x;
+    private int y;
+    private int decX;
+    private int decY;
+    private int rectDirection = 0;   //0=rechts, 1=unten, 2=links, 3=oben
 
     public MainGame(String title) {
         super(title);
@@ -12,17 +16,46 @@ public class MainGame extends BasicGame {
 
     @Override
     public void init(GameContainer gc) throws SlickException {
-    // wird einmal aufgerufen am anfang
-        this.x = 100;
-        this.y = 100;
+        // wird einmal aufgerufen am anfang
+        this.x = 50;
+        this.y= 50;
+
     }
 
     @Override
     public void update(GameContainer gc, int delta) throws SlickException {
         // delta = Zeit seit letztem Aufruf
-        this.x++;
 
-    }
+        if (this.rectDirection == 0) {
+            this.x++;
+            if (x > 750) {
+                this.rectDirection = 1;
+            }
+        }
+        else if (this.rectDirection == 1) {
+            this.y++;
+            if (y > 550) {
+                this.rectDirection = 2;
+            }
+        } else if (this.rectDirection == 2) {
+            this.x--;
+            if (x < 50) {
+                this.rectDirection = 3;
+            }
+        } else if (this.rectDirection == 3) {
+            this.y--;
+            if (y > 50) {
+                this.rectDirection = 0;
+            }
+
+
+        }
+
+
+
+
+
+}
 
     @Override
     public void render(GameContainer gc, Graphics graphics) throws SlickException {
